1d19de2a17f0ff75ef5926b174c8c972ff9ba8e9
Albert Jennifer <35313442+HAJennifer@users.noreply.github.com>
2018-05-28 17:59:00 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,49 +1,61 @@
+
+var score = 0;
+document.getElementById('playerScore').innerHTML = score;
+
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-    
+
+class Enemy{
+
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-        this.sprite = 'images/enemy-bug.png';
-        this.x = x;
-        this.y = y;
-        this.speed = speed;
-    };
+       constructor(x, y, speed) {
+            this.x = x;
+            this.y = y;
+            this.speed = speed;
+            this.sprite = 'images/enemy-bug.png';
+            this.width = 70;
+            this.height = 50;
+    }
+        
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-      this.x += this.speed * dt;
-
-    if (this.x > 505) {
-        this.x = -10;
+  update(dt) {
+    this.x+=this.speed*dt;
+     if (this.x >= 505) {
+        this.x = 0;
     }
-};
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y){
+class Player{
+    constructor(x, y) {
     this.x = x;
     this.y = y;
+    this.speed = speed;
     this.sprite = 'images/char-boy.png';
-};
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 
-Player.prototype.handleInput = function(key) {
+update(dt) {}
+
+render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+handleInput(key) {
 
     if (key === 'left') {
         this.x = this.x - 101;
@@ -61,20 +73,21 @@ Player.prototype.handleInput = function(key) {
     }
 
 
+}
+
 };
 
-Player.prototype.update = function(dt) {}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
 
-var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256, 'images/enemy-bug.png');
-
-allEnemies.push(enemy);
-
+var allEnemies = [
+    new Enemy(0, 40),
+    new Enemy(0, 130),
+    new Enemy(0, 220)
+];
 
 var player = new Player(200, 400);
 
